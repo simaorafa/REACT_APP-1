a3feca5f606123f5aeab1d9f6e5d0e480dc61ca6
canja <a14813@oficina.pt>
2026-05-21 09:37:04 +0000

    TOMTOMA COMITADA

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.jsx'
-import Message from './Message.jsx'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+//import Message from './Message.jsx'
+//import Header from './Header.jsx'
+//import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <Message/>
     <App/>
-    <Footer />
   </StrictMode>,
 )
 
